dcbf4307beea14879b39e764acb3231972ddcbd9
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:13:55 +0300

    Created the formatting startegy that is speacialized for LogFormatter

---
+++ LogService/LogService.Formatting/Formatting.FormattingStrategies/FormatingLogMessageStrategy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using LogService.Enums;
+
+namespace LogService.Formatting.FormattingStrategies
+{ 
+	public class FormatingLogMessageStrategy : IFormateStrategy
+	{
+		protected  string _sep = " ";
+		protected bool _isDataTimeGiven = false;
+		public string Sep { get => _sep; set => _sep = value; }
+
+		public object Formate(params object[] param)
+		{
+			string result = "";
+			foreach(var item in param)
+			{
+				if (item is DateTime dateTime)
+				{
+					// Update datetime is given to true
+					_isDataTimeGiven = true;
+					
+					// Append to restul
+					result += FormateDateTime(dateTime) + _sep;
+				}
+				else if (item is MessageType msType)
+				{
+					result += $"[{msType}]" + _sep;
+				}
+				else
+				{
+					result += item.ToString() + _sep;
+				}
+			}
+
+			// Check if Datatime isn't given
+			if (!_isDataTimeGiven) 
+			{
+				// Add it to the begginge of the restul
+				result = FormateDateTime(DateTime.Now) + _sep + result;
+			}
+
+
+			// Iterate over them and join then finally return result
+			foreach (var item in param)
+			{
+				result += (item.ToString() + _sep);
+			}
+
+			// Return result trimmed
+			if (char.TryParse(_sep, out char sepAsChar))
+			{
+				return result.TrimEnd(sepAsChar);
+			}
+			else
+			{
+				return result.Trim(_sep.ToCharArray());
+			}
+		}
+
+
+		///
+		protected string FormateDateTime(DateTime dateTime)
+		{
+			return dateTime.ToString("yyyy-MM-dd HH:mm:ss.fff");
+		}
+	}
+}
