a2688d87604be2e41fc8e437004e15a7b4f06aa9
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:21:45 +0300

    Added validation result which will be returned each time method from the validator is called (dependency call)

---
+++ LogService/LogService.Validation/Validation.Core/ValidationResult.cs
@@ -0,0 +1,23 @@
+﻿namespace LogService.Validation.Core 
+{ 
+	/// <summary>
+	/// Immutable object
+	/// </summary>
+	public class ValidationResult
+	{
+		/* Instance attributes */
+		private readonly bool _isValid;
+        private readonly bool _exists;
+
+        /* Constructors */
+        public ValidationResult(bool validationResult=false, bool exists=false)
+        {
+            _isValid = validationResult;
+            _exists = exists;
+        }
+
+        /* Setters and getters */
+        public bool IsValid { get { return _isValid; } }
+        public bool Exists { get { return _exists; } }
+    }
+}
