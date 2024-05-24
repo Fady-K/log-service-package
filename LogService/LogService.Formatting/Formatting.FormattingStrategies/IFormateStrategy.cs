732275c3f38ea3d30f585a9bb47891a16b5d1f48
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:12:57 +0300

    Added IFormateStrategy which is the interface that all other startegies implement

---
+++ LogService/LogService.Formatting/Formatting.FormattingStrategies/IFormateStrategy.cs
@@ -0,0 +1,8 @@
+﻿namespace LogService.Formatting.FormattingStrategies
+{
+	public interface IFormateStrategy
+	{
+        string Sep { get; set; }
+        object Formate(params object[] param);
+	}
+}
