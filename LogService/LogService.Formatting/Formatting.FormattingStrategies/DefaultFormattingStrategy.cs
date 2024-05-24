7cb30618866429da63c172ef98a524881ed9cf2f
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:12:13 +0300

    Created default formatting startetgy that is used, incase any concrete class doesn't implement any other startegy

---
+++ LogService/LogService.Formatting/Formatting.FormattingStrategies/DefaultFormattingStrategy.cs
@@ -0,0 +1,42 @@
+﻿namespace LogService.Formatting.FormattingStrategies
+{
+	public class DefaultFormattingStrategy : IFormateStrategy
+	{
+		/* Insatnce attributes */
+		protected string _sep;
+
+        /* Constructors */
+        public DefaultFormattingStrategy()
+        {
+			_sep = "-";
+        }
+
+        /* Setters and getters */
+		public string Sep { get => _sep; set => _sep = value; }
+
+
+		/* Instance methods */
+        public object Formate(params object[] param)
+		{
+			// Just append all params into string (join them) and trim the output, finally return it.
+			string result = "";
+
+			// Iterate over them and join then finally return result
+			foreach(var item in param)
+			{
+				result += (item.ToString() + _sep);
+			}
+
+			// Return result trimmed
+			if (char.TryParse(_sep, out char sepAsChar))
+			{
+				return result.TrimEnd(sepAsChar);
+			}
+			else
+			{
+				return result.Trim(_sep.ToCharArray());
+			}
+
+		}
+	}
+}
