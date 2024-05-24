68a4328760ebcb39074ebb64fef356c0150b0b96
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:15:25 +0300

    Added enums related to validation subService

---
+++ LogService/LogService.Validation/Validation.Enums/ValidationStartegyType.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogService.Validation.Enums
+{
+    public enum ValidationStartegyType
+    {
+        YoutubeURLValidation,
+        DownloadPathValidation
+    }
+}
