63ea7d60aaef69a9a13605a7a4011d2efe104106
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:09:42 +0300

    Added AbstractFormatter which will be extended and implemented by LogMessageFormatter

---
+++ LogService/LogService.Formatting/Formatting.Core/AbstractFormatter.cs
@@ -0,0 +1,41 @@
+﻿using LogService.Formatting.FormattingStrategies;
+using System;
+
+namespace LogService.Formatting.Core
+{
+	public abstract class AbstractFormatter
+	{
+		/* Fields */
+		protected IFormateStrategy _formateStrategy;
+
+
+        /* Constructors */
+        public AbstractFormatter()
+        {
+            _formateStrategy = new DefaultFormattingStrategy();
+        }
+
+        public AbstractFormatter(IFormateStrategy formateStrategy)
+        {
+            _formateStrategy = formateStrategy;
+        }
+
+
+        /* Setters and getters */
+        public IFormateStrategy FormateStrategy { get => _formateStrategy; set => _formateStrategy = value; }
+        
+
+        /* Instance methods */
+        public object Formate(params object[] param)
+        {
+            try
+            {
+                return _formateStrategy.Formate(param);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
