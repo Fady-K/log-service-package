6cd06fe87e43515ecfb2ab0f0662243cd078e0a4
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:16:14 +0300

    Created a custom exception class for this specializeed nested subservice

---
+++ LogService/LogService.Validation/Validation.Exceptions/ValidationException.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace LogService.Validation.Exceptions
+{
+	public class ValidationException : Exception
+	{
+        /* Constants */
+        public const string EXCEPTION_SUFFIX = nameof(ValidationException) + ": ";
+
+
+        /* Instance attributes */
+        private string _msg;
+
+
+        /* Constructors */
+        public ValidationException(string message)             // Paramerized constructor
+		{
+			InitMessageAttr(message);
+		}
+
+
+        /* Getters */
+        public override string Message => _msg;
+
+
+
+        //////////////////////////////////////// Helper methods  //////////////////////////////////////
+        private void InitMessageAttr(string message)
+        {
+			// Add this suffix to every excpetion if doesn't exists
+			if (!message.StartsWith(EXCEPTION_SUFFIX))
+			{
+				// Add the suffex
+				message = EXCEPTION_SUFFIX + message;
+			}
+
+			// Init instance attribute
+			_msg = message;
+		}
+	}
+}
