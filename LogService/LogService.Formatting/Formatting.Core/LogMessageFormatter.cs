785668a913ae278b2bd563925afafc314de3d8b0
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:11:16 +0300

    Added concrete class that implements the AbstractFormatter, and Formattes the log message using different startegies definned in startegies namespace

---
+++ LogService/LogService.Formatting/Formatting.Core/LogMessageFormatter.cs
@@ -0,0 +1,12 @@
+﻿using LogService.Formatting.FormattingStrategies;
+
+namespace LogService.Formatting.Core
+{
+	public class LogMessageFormatter : AbstractFormatter
+	{
+        public LogMessageFormatter()
+        {
+            _formateStrategy = new FormatingLogMessageStrategy();
+        }
+    }
+}
