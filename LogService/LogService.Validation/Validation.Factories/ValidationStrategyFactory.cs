3bcb6ca24be4b17685e11774b2f0a0daeba54c3a
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 22:17:24 +0300

    Added validation startegy factory which is factory that return specific validation startegy based on passed argument (Factory method design patter)

---
+++ LogService/LogService.Validation/Validation.Factories/ValidationStrategyFactory.cs
@@ -0,0 +1,24 @@
+﻿using LogService.Validation.Enums;
+using LogService.Validation.Strategies;
+
+namespace LogService.Validation.Factories
+{
+    public class ValidationStrategyFactory
+	{
+		public static IValidationStrategy Create(ValidationStartegyType strategyType)
+		{
+			if (strategyType.Equals(ValidationStartegyType.DownloadPathValidation))
+			{
+				return null;
+			}
+			else if (strategyType.Equals(ValidationStartegyType.YoutubeURLValidation))
+			{
+				return null;
+			}
+			else
+			{
+				return null;
+			}
+		}
+	}
+}
