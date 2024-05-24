fcacf80f5261bfe8224b739be798a016171ab7b8
Fady-K <popfadykamal151617@gmail.com>
2024-05-24 21:04:47 +0300

    created and implemeted logger.cs (concrete class that implementes Abstract Logger)

---
+++ LogService/LogService.Core/Logger.cs
@@ -0,0 +1,15 @@
+﻿using LogService.Core.LogStrategies;
+using LogService.Formatting.Core;
+
+namespace LogService.Core
+{
+	public class Logger : AbstractLogger
+	{
+        public Logger(ILogStrategy logStartegy, AbstractFormatter formatter) : base(logStartegy, formatter)
+        {
+            
+        }
+        /////////////////////////////////////////////// Helper methods ////////////////////////////
+        ///
+    }
+}
