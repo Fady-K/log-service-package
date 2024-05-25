c7b64db04cb7feb739e472237f759f538697f57d
Fady-K <popfadykamal151617@gmail.com>
2024-05-26 01:51:01 +0300

    documented rest of the interface

--- LogService/LogService.Core/LogStartegies/ILogStrategy.cs
+++ LogService/LogService.Core/LogStartegies/ILogStrategy.cs
@@ -3,13 +3,21 @@
 	/// <summary>
 	/// The ILogStartegy Interface that provides an interface for different startegies.
 	/// <remarks>This ILogStrategy is implemented by differnet concrete startegies</remarks>
-	/// <seealso cref="AccumulativeMessageLoggingStrategy"/>
+	/// <seealso cref="AccumulativeMessageBasedThresholdLoggingStrategy"/>
 	/// <seealso cref="DefaultLogStrategy"/>
 	/// <seealso cref="InstanteMessageLoggingStrategy"/>
 	/// </summary>
 	public interface ILogStrategy
 	{
+		/// <summary>
+		/// Takes a message to log it.
+		/// </summary>
+		/// <param name="message">Message that is about to get logged.</param>
 		void Log(string message);
+
+		/// <summary>
+		/// Property for validLogFilePath.
+		/// </summary>
 		string ValidLogFilePath { get; set; }
 	}
 }
