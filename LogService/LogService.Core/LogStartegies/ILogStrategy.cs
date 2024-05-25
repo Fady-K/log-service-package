2524aca192464d87ddc829d22e7c7d94a0af81d8
Fady-K <popfadykamal151617@gmail.com>
2024-05-25 12:40:59 +0300

    Document the class header

--- LogService/LogService.Core/LogStartegies/ILogStrategy.cs
+++ LogService/LogService.Core/LogStartegies/ILogStrategy.cs
@@ -1,5 +1,12 @@
 ﻿namespace LogService.Core.LogStrategies
 {
+	/// <summary>
+	/// The ILogStartegy Interface that provides an interface for different startegies.
+	/// <remarks>This ILogStrategy is implemented by differnet concrete startegies</remarks>
+	/// <seealso cref="AccumulativeMessageLoggingStrategy"/>
+	/// <seealso cref="DefaultLogStrategy"/>
+	/// <seealso cref="InstanteMessageLoggingStrategy"/>
+	/// </summary>
 	public interface ILogStrategy
 	{
 		void Log(string message);
