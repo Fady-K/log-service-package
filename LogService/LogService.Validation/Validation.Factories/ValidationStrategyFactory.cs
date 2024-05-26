afc71d7c9676271a2a70d190a0dac32996181963
Fady-K <popfadykamal151617@gmail.com>
2024-05-26 17:20:14 +0300

    Documented the factory

--- LogService/LogService.Validation/Validation.Factories/ValidationStrategyFactory.cs
+++ LogService/LogService.Validation/Validation.Factories/ValidationStrategyFactory.cs
@@ -3,8 +3,18 @@ using LogService.Validation.Strategies;
 
 namespace LogService.Validation.Factories
 {
-    public class ValidationStrategyFactory
+	/// <summary>
+	/// The ValidationStrategyFactory class is a factory class that creates instances of IValidationStrategy.
+	/// It implements the Factory Method design pattern.
+	/// </summary>
+	public class ValidationStrategyFactory
 	{
+		/// <summary>
+		/// Creates an instance of IValidationStrategy based on the provided strategy type.
+		/// </summary>
+		/// <param name="strategyType">The type of the validation strategy to create.</param>
+		/// <returns>Returns an instance of IValidationStrategy. If the strategy type does not match any known types, it returns null.</returns>
+		/// <seealso cref="IValidationStrategy"/>
 		public static IValidationStrategy Create(ValidationStartegyType strategyType)
 		{
 			if (strategyType.Equals(ValidationStartegyType.LogFilePathValidation))
