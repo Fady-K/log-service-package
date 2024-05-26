b789cb222d188029f69f4c16b01ddd9fb748d168
Fady-K <popfadykamal151617@gmail.com>
2024-05-26 17:30:58 +0300

    Added init documentation

--- LogService/LogService.Validation/Validation.Core/ValidationResult.cs
+++ LogService/LogService.Validation/Validation.Core/ValidationResult.cs
@@ -1,23 +1,43 @@
 ﻿namespace LogService.Validation.Core 
-{ 
+{
 	/// <summary>
-	/// Immutable object
+	/// The ValidationResult class is an immutable object that represents the result of a validation operation.
+	/// It contains two properties: IsValid and Exists.
 	/// </summary>
 	public class ValidationResult
 	{
 		/* Instance attributes */
+		/// <summary>
+		/// Indicates whether the validation was successful.
+		/// </summary>
 		private readonly bool _isValid;
-        private readonly bool _exists;
 
-        /* Constructors */
-        public ValidationResult(bool validationResult=false, bool exists=false)
-        {
-            _isValid = validationResult;
-            _exists = exists;
-        }
+		/// <summary>
+		/// Indicates whether the validated object exists.
+		/// </summary>
+		private readonly bool _exists;
 
-        /* Setters and getters */
-        public bool IsValid { get { return _isValid; } }
-        public bool Exists { get { return _exists; } }
-    }
+		/* Constructors */
+		/// <summary>
+		/// Initializes a new instance of the ValidationResult class with the specified validation result and existence.
+		/// </summary>
+		/// <param name="validationResult">Indicates whether the validation was successful. The default value is false.</param>
+		/// <param name="exists">Indicates whether the validated object exists. The default value is false.</param>
+		public ValidationResult(bool validationResult = false, bool exists = false)
+		{
+			_isValid = validationResult;
+			_exists = exists;
+		}
+
+		/* Setters and getters */
+		/// <summary>
+		/// Gets a value indicating whether the validation was successful.
+		/// </summary>
+		public bool IsValid { get { return _isValid; } }
+
+		/// <summary>
+		/// Gets a value indicating whether the validated object exists.
+		/// </summary>
+		public bool Exists { get { return _exists; } }
+	}
 }
