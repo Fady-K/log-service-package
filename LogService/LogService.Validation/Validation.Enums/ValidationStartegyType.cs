a7596a157b8e2b77f8d8f49df214b0c077374692
Fady-K <popfadykamal151617@gmail.com>
2024-05-26 17:32:31 +0300

    Added documentation

--- LogService/LogService.Validation/Validation.Enums/ValidationStartegyType.cs
+++ LogService/LogService.Validation/Validation.Enums/ValidationStartegyType.cs
@@ -1,8 +1,18 @@
 ﻿namespace LogService.Validation.Enums
 {
-    public enum ValidationStartegyType
-    {
-        LogFilePathValidation,
-        LogFilePathWithoutExceptionsValidation
-    }
+	/// <summary>
+	/// The ValidationStartegyType enumeration provides identifiers for different types of validation strategies.
+	/// </summary>
+	public enum ValidationStartegyType
+	{
+		/// <summary>
+		/// Represents the LogFilePathValidation strategy type.
+		/// </summary>
+		LogFilePathValidation,
+
+		/// <summary>
+		/// Represents the LogFilePathWithoutExceptionsValidation strategy type.
+		/// </summary>
+		LogFilePathWithoutExceptionsValidation
+	}
 }
