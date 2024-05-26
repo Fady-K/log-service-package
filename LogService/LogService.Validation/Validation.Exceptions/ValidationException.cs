f0755c56bf182a21e6de164f278b816e865d9b7d
Fady-K <popfadykamal151617@gmail.com>
2024-05-26 17:31:16 +0300

    Documented the validation exception class

--- LogService/LogService.Validation/Validation.Exceptions/ValidationException.cs
+++ LogService/LogService.Validation/Validation.Exceptions/ValidationException.cs
@@ -2,30 +2,53 @@
 
 namespace LogService.Validation.Exceptions
 {
+	/// <summary>
+	/// The ValidationException class represents errors that occur during application execution related to validation.
+	/// This class is derived from the System.Exception class.
+	/// </summary>
 	public class ValidationException : Exception
 	{
-        /* Constants */
-        public const string EXCEPTION_SUFFIX = nameof(ValidationException) + ": ";
+		/* Constants */
+		/// <summary>
+		/// The suffix added to every exception message.
+		/// </summary>
+		public const string EXCEPTION_SUFFIX = nameof(ValidationException) + ": ";
 
 
-        /* Instance attributes */
-        private string _msg;
+		/* Instance attributes */
+		/// <summary>
+		/// The message that describes the error.
+		/// </summary>
+		private string _msg;
 
 
-        /* Constructors */
-        public ValidationException(string message)             // Paramerized constructor
+		/* Constructors */
+		/// <summary>
+		/// Initializes a new instance of the ValidationException class with a specified error message.
+		/// </summary>
+		/// <param name="message">The message that describes the error.</param>
+		public ValidationException(string message)             // Paramerized constructor
 		{
 			InitMessageAttr(message);
 		}
 
 
-        /* Getters */
-        public override string Message => _msg;
+		/* Getters */
+
+		/// <summary>
+		/// Gets a message that describes the current exception.
+		/// </summary>
+		public override string Message => _msg;
 
 
 
-        //////////////////////////////////////// Helper methods  //////////////////////////////////////
-        private void InitMessageAttr(string message)
+		//////////////////////////////////////// Helper methods  //////////////////////////////////////
+		/// <summary>
+		/// Initializes the _msg field with the provided message.
+		/// If the message does not start with the EXCEPTION_SUFFIX, it adds the suffix to the message.
+		/// </summary>
+		/// <param name="message">The message to initialize the _msg field with.</param>
+		private void InitMessageAttr(string message)
         {
 			// Add this suffix to every excpetion if doesn't exists
 			if (!message.StartsWith(EXCEPTION_SUFFIX))
