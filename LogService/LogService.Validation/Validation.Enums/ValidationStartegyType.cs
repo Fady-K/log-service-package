801b426a647d7d8d4cd8a7f826ec4cdb86bec406
Fady-K <popfadykamal151617@gmail.com>
2024-05-26 17:30:27 +0300

    removed redundant code

--- LogService/LogService.Validation/Validation.Enums/ValidationStartegyType.cs
+++ LogService/LogService.Validation/Validation.Enums/ValidationStartegyType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LogService.Validation.Enums
+﻿namespace LogService.Validation.Enums
 {
     public enum ValidationStartegyType
     {
