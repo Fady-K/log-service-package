60996ded300f0d8606238d60bfad768106f88dbc
Fady-K <popfadykamal151617@gmail.com>
2024-07-15 22:34:42 +0300

    Corrected the containing directory name from 'LogStartegies to LogStrategies'

---
+++ LogService/LogService.Core/LogStrategies/ILogStrategy.cs
@@ -0,0 +1,22 @@
+﻿namespace LogService.Core.LogStrategies
+{
+	/// <summary>
+	/// The ILogStartegy Interface that provides an interface for different startegies.
+	/// <remarks>This ILogStrategy is implemented by differnet concrete startegies</remarks>
+	/// <seealso cref="AccumulativeMessageBasedThresholdLoggingStrategy"/>
+	/// <seealso cref="InstanteMessageLoggingStrategy"/>
+	/// </summary>
+	public interface ILogStrategy
+	{
+		/// <summary>
+		/// Takes a message to log it.
+		/// </summary>
+		/// <param name="message">Message that is about to get logged.</param>
+		void Log(string message);
+
+		/// <summary>
+		/// Property for validLogFilePath.
+		/// </summary>
+		string ValidLogFilePath { get; set; }
+	}
+}
---
+++ LogService/LogService.Core/LogStrategies/InstanteMessageLoggingStrategy.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+
+namespace LogService.Core.LogStrategies
+{
+	/// <summary>
+	/// The InstantMessageLoggingStartegy.
+	/// <remarks>This startegy implements the ILogStrategy interface, and takes log message (log implemeneted) and then log it instantly.</remarks>
+	/// <seealso cref="ILogStrategy"/>
+	/// <seealso cref="Log(string)"/>
+	/// </summary>
+	public class InstanteMessageLoggingStrategy: ILogStrategy
+	{
+		/* Instance attributes */
+
+		/// <summary>
+		/// The valid log file path
+		/// </summary>
+		protected string _validLogFilePath;
+
+
+        /* Constructors */
+		/// <summary>
+		/// Parametrized constructor of InstantMessageLoggingStrategy.
+		/// </summary>
+		/// <remarks>Takes validLogFilePath</remarks>
+		/// <param name="validLogFilePath">Valid path of the log file</param>
+        public InstanteMessageLoggingStrategy(string validLogFilePath)
+        {
+			// Init local attributes
+			ValidLogFilePath = validLogFilePath;
+        }
+
+
+		/* Setters and getters */
+		/// <summary>
+		/// Property for validLogFilePath.
+		/// </summary>
+		public string ValidLogFilePath { get => _validLogFilePath; set => _validLogFilePath = value; }
+
+
+		/* Instance methos */
+
+		/// <summary>
+		/// Takes a message to log it.
+		/// </summary>
+		/// <param name="message">Message that is about to get logged.</param>
+		/// <exception cref="System.Exception"></exception>
+		public void Log(string message)
+		{
+			try
+			{
+				// Log using stream writter
+				using (StreamWriter sw = File.AppendText(_validLogFilePath))
+				{
+					// write message
+					sw.WriteLine(message);
+				}
+			}
+			catch(Exception ex)
+			{
+				throw new Exception(ex.Message);
+			}
+
+		}
+	}
+}
