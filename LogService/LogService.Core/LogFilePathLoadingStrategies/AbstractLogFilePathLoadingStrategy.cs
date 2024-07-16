c3b69affa218fdfc48b0516a88456c244ff18fcd
Fady-K <popfadykamal151617@gmail.com>
2024-07-16 20:55:10 +0300

    Documented members of AbstractLogFilePathLoadingStrategy.cs (Abstract Class)

--- LogService/LogService.Core/LogFilePathLoadingStrategies/AbstractLogFilePathLoadingStrategy.cs
+++ LogService/LogService.Core/LogFilePathLoadingStrategies/AbstractLogFilePathLoadingStrategy.cs
@@ -1,26 +1,68 @@
 ﻿namespace LogService.LogService.LogService.Core.LogFilePathLoadingStrategies
 {
+	/// <summary>
+	/// The AbstractLogFilePathLoadingStrategy class.
+	/// This abstract class provides the strategy for loading log file paths.
+	/// </summary>
+	/// <remarks>
+	/// This class follows the Strategy design pattern to allow different mechanisms for loading log file paths to be used interchangeably.
+	/// </remarks>
+	/// <seealso cref="LoadLogFilePathFromXMLAppConfig"/>
 	public abstract class AbstractLogFilePathLoadingStrategy
 	{
 		/* Instance Attributes */
+
+		/// <summary>
+		/// The path of the log file.
+		/// </summary>
 		protected string __logFilePath;
+
+		/// <summary>
+		/// The default path of the log file.
+		/// </summary>
 		protected string __defaultLogFilePath;
 
+
 		/* Constructors */
+
+		/// <summary>
+		/// Initializes a new instance of the AbstractLogFilePathLoadingStrategy class.
+		/// </summary>
 		public AbstractLogFilePathLoadingStrategy()
 		{
-			// Init Instance attributese 
+			// Init instance attributes 
 			__logFilePath = string.Empty;
 			__defaultLogFilePath = string.Empty;
 		}
 
 
 		/* Setters and getters (properties) */
+
+		/// <summary>
+		/// Gets the path of the log file.
+		/// </summary>
 		public string LogFilePath => __logFilePath;
+
+		/// <summary>
+		/// Gets the default path of the log file.
+		/// </summary>
 		public string DefaultLogFilePath => __defaultLogFilePath;
 
+
 		/* Instance Methods */
+
+		/// <summary>
+		/// Loads the path of the log file.
+		/// </summary>
+		/// <param name="configFile">The configuration file.</param>
+		/// <param name="key">The key to retrieve the log file path.</param>
 		protected abstract void LoadLogFilePath(string configFile, string key);
+
+		/// <summary>
+		/// Loads the default path of the log file.
+		/// </summary>
+		/// <param name="configFile">The configuration file.</param>
+		/// <param name="key">The key to retrieve the default log file path.</param>
 		protected abstract void LoadDefaultLogFilePath(string configFile, string key);
 	}
 }
