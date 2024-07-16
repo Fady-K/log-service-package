f89cfb95ccd75ec3331ee30cbd71ca0965166f0f
Fady-K <popfadykamal151617@gmail.com>
2024-07-16 20:52:03 +0300

    Created the log file loading strategy (Abstract class)

---
+++ LogService/LogService.Core/LogFilePathLoadingStrategies/AbstractLogFilePathLoadingStrategy.cs
@@ -0,0 +1,26 @@
+﻿namespace LogService.LogService.LogService.Core.LogFilePathLoadingStrategies
+{
+	public abstract class AbstractLogFilePathLoadingStrategy
+	{
+		/* Instance Attributes */
+		protected string __logFilePath;
+		protected string __defaultLogFilePath;
+
+		/* Constructors */
+		public AbstractLogFilePathLoadingStrategy()
+		{
+			// Init Instance attributese 
+			__logFilePath = string.Empty;
+			__defaultLogFilePath = string.Empty;
+		}
+
+
+		/* Setters and getters (properties) */
+		public string LogFilePath => __logFilePath;
+		public string DefaultLogFilePath => __defaultLogFilePath;
+
+		/* Instance Methods */
+		protected abstract void LoadLogFilePath(string configFile, string key);
+		protected abstract void LoadDefaultLogFilePath(string configFile, string key);
+	}
+}
